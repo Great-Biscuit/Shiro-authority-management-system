78649c028f66c0e299691a513f0f7e73948ab4f7
GreatBiscuit <318215104@qq.com>
2021-05-30 19:26:36 +0800

    项目创建测试

---
+++ src/main/java/com/csuft/shiroauthoritymanagementsystem/ShiroAuthorityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.csuft.shiroauthoritymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroAuthorityManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroAuthorityManagementSystemApplication.class, args);
+    }
+
+}
