f03a5cd8eba34c1b04661b44a477acd295c8dcac
北极熊的浮冰 <1403368396@qq.com>
2021-06-08 19:34:03 +0800

    test

--- src/main/java/com/qfedu/dtboot/aop/SysLogAspect.java
+++ src/main/java/com/qfedu/dtboot/aop/SysLogAspect.java
@@ -22,21 +22,30 @@ import java.util.Date;
 /**
  * Created by helen on 2018/3/9
  * 系统日志：切面处理类
+ * 备注：2021-06-08 增加了部分注释（lmz）
  */
-@Aspect
-@Component
+@Aspect //定义切面类，把当前类标识为一个切面供容器读取
+@Component  //声明组件
 public class SysLogAspect {
-
+    /*
+    Component注解相当于配置了
+    <bean class="com.qfedu.dtboot.aop.SysLogAspect“></bean>
+    */
     @Autowired
     private SysLogService sysLogService;
 
-    //定义@Pointcut
-    //@annotation：表示注解出现的位置切入代码
+    /*定义@Pointcut，指明切入点
+     *@annotation：表示注解出现的位置切入代码
+     *匹配com.qfedu.dtboot.annotation.MyLog包下所有类、方法的执行作为切入点
+     */
     @Pointcut("@annotation(com.qfedu.dtboot.annotation.MyLog)")
     public void logPointCut(){
 
     }
-
+    /*AfterReturning：后置增强
+    相当于AfterReturningAdvice
+    方法正常退出时执行
+    */
     @AfterReturning("logPointCut()")
     public void saveSysLog(JoinPoint joinPoint){
 
@@ -49,7 +58,9 @@ public class SysLogAspect {
         MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
         Method method = methodSignature.getMethod();
 
-        //操作
+        /*操作
+         *返回该元素的指定类型的注释，如果是这样的注释，否则返回null。
+         */
         MyLog mylog = method.getAnnotation(MyLog.class);
         if(mylog != null){
             String value = mylog.value();
@@ -58,6 +69,7 @@ public class SysLogAspect {
 
         //请求的方法名
         String className = joinPoint.getTarget().getClass().getName();
+        System.out.println(className);
         String methodName = method.getName();
         sysLog.setMethod(className + "." + methodName);
 
--- src/main/java/com/qfedu/dtboot/config/KaptchaConfig.java
+++ src/main/java/com/qfedu/dtboot/config/KaptchaConfig.java
@@ -17,8 +17,11 @@ public class KaptchaConfig {
     @Bean
     public DefaultKaptcha producer() {
         Properties properties = new Properties();
+        //去除验证码的边框(black)
         properties.put("kaptcha.border", "no");
+        //验证码字体的颜色
         properties.put("kaptcha.textproducer.font.color", "black");
+        //验证码生成的字母数量
         properties.put("kaptcha.textproducer.char.length", "1");
         Config config = new Config(properties);
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
